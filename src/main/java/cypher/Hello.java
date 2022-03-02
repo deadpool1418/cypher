83919b5db588bada00788c13f4a834592074532c
Vijay <vachaudhari26@gmail.com>
2022-03-02 11:57:41 +0530

    Files Modified

---
+++ src/main/java/cypher/Hello.java
@@ -0,0 +1,38 @@
+
+
+import java.io.IOException;
+import jakarta.*;
+
+
+/**
+ * Servlet implementation class Hello
+ */
+
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 16549879L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
